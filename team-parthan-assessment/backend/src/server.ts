8b3434840ce388542ee4a7908799f417c7dcc86b
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2025-07-04 12:26:51 +0530

    cors fix 2

--- team-parthan-assessment/backend/src/server.ts
+++ team-parthan-assessment/backend/src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 // app.use(cors());
 app.use(cors(
   {
-    origin: 'https://cps-ten.vercel.app/', // Adjust this to your frontend URL
+    origin: 'https://cps-ten.vercel.app', // Adjust this to your frontend URL
     credentials: true, // Allow credentials if needed
   }
 ));
